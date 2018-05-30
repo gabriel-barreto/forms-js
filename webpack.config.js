9ea147d711813d89049147e0cae92c5db1349622
Gabriel Barreto <barreto-gabriel@outlook.com>
2018-05-30 01:16:28 -0300

    Add UMD support

--- webpack.config.js
+++ webpack.config.js
@@ -2,41 +2,44 @@ const path = require('path');
 const webpack = require('webpack');
 
 const config = {
-    entry: './index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: './dist/',
-        filename: 'index.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: [ 'babel-loader' ],
-                exclude: /node_modules/,
-            }
-        ]
-    },
+  mode: process.env.NODE_ENV || 'development',
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: './dist/',
+    libraryTarget: 'umd',
+    library: 'forms',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
 };
 
 if (process.env.NODE_ENV === 'production') {
-    module.exports.devtool = '#sorce-map';
-    module.exports.plugins = (module.exports.plugins || []).concat([
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: '"production"'
-            }
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            sourceMap: true,
-            compress: {
-                warnings: false
-            }
-        }),
-        new webpack.LoaderOptionsPlugin({
-            minimize: true
-        })
-    ])
+  module.exports.devtool = '#sorce-map';
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"',
+      },
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false,
+      },
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+    }),
+  ]);
 }
 
 module.exports = config;
